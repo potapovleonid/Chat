94d09abf2bea1b0211a8c246cee8cfd4f98bfffb
Leonid <kissmykrabeskill@gmail.com>
2022-08-30 23:30:12 +0500

    Added visible start\stop buttons

--- chat-server/src/ru/home/des/chat/server/gui/ServerGUI.java
+++ chat-server/src/ru/home/des/chat/server/gui/ServerGUI.java
@@ -46,6 +46,8 @@ public class ServerGUI extends JFrame implements ActionListener, Thread.Uncaught
         btnStart.addActionListener(this);
         btnStop.addActionListener(this);
 
+        setVisibleStart(true);
+
         setVisible(true);
     }
 
@@ -53,9 +55,9 @@ public class ServerGUI extends JFrame implements ActionListener, Thread.Uncaught
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         if (source == btnStart) {
-            chatServer.start(8181);
+            setVisibleStart(!chatServer.start(8181));
         } else if (source == btnStop) {
-            chatServer.stop();
+            setVisibleStart(chatServer.stop());
         } else
             throw new RuntimeException("Unknown source: " + source);
     }
@@ -79,5 +81,10 @@ public class ServerGUI extends JFrame implements ActionListener, Thread.Uncaught
             log.append(msg + "\n");
             log.setCaretPosition(log.getDocument().getLength());
         });
+    }
+
+    public void setVisibleStart(boolean visible){
+        btnStart.setVisible(visible);
+        btnStop.setVisible(!visible);
     }
 }
